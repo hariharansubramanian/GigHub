5b68f1eab94f779cacbebfa48badda8f5b8c4863
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-11-14 13:51:53 +0530

    Added Authorization to controller methods Implemented Create POST method to save a Gig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,14 +16,33 @@ namespace GigHub.Controllers
             _dbContext = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
             {
                 Genres = _dbContext.Genres.ToList()
             };
-
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _dbContext.Users.Single(u => u.Id == artistId);
+            var genre = _dbContext.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _dbContext.Gigs.Add(gig);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
