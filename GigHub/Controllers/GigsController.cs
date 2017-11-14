4fb659799f7615f76eb0c282e58a7de2adc2f468
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-11-14 13:07:13 +0530

    Created a View model 'GigFormViewModel' to be used by the Create.cshtml view Using dbContext to retrieve Genres from Database and serve it to the Genres dropdown in the view

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public GigsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
