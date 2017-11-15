765c59794c333b70fe4123de4b7bf8e8febfe50f
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-11-15 17:15:09 +0530

    Added gigs,day,month,date,details divs and added custom CSS for each in the Upcoming Gigs view Index.cshtml

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace GigHub.Controllers
         {
             var upcomingGigs = _dbContext.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcomingGigs);
         }
