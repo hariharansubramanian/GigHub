86e4f49011c5a59063200c713f486c587cf77cbf
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-11-15 16:08:06 +0530

    Deleted defaut content from Index page Retriving all upcoming gigs from database and passing it to view Iterating through each gig and displaying in a an Unordered List

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _dbContext.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
